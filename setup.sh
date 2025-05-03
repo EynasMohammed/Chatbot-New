d48891c126827e895d30e7d6433e7e3ac07ae703
EynasMohammed <eynasmohammedalnahary@gmail.com>
2025-05-04 00:57:00 +0300

    add stage6+terraform

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ $# -ne 4 ]; then
-    echo "Usage: $0 <PAT_token> <repo_url> <branch_name> <key_vault_name>"
+if [ $# -ne 5 ]; then
+    echo "Usage: $0 <PAT_token> <repo_url> <branch_name> <password> <DB_host>"
     exit 1
 fi
 
@@ -10,10 +10,30 @@ fi
 PAT_TOKEN="$1"
 REPO_URL="$2"
 BRANCH_NAME="$3"
+PASSWORD="$4"
 REPO_NAME=$(basename "$REPO_URL" .git)
 USER=$(whoami)
 HOME_DIR=$(eval echo ~$USER)
-KEY_VAULT_NAME="$4"
+DB_HOST="$5"
+
+# Set up PostgreSQL database
+echo "Setting up database..."
+echo "$DB_HOST:5432:postgres:azureadmin:$PASSWORD" > $HOME/.pgpass
+chmod 600 $HOME/.pgpass
+if ! sudo -u postgres psql -lqt | cut -d \| -f 1 | grep -qw project; then
+    sudo -u postgres psql -c "CREATE DATABASE project"
+else
+    echo "Database 'project' already exists"
+fi
+sudo -u postgres psql -d project -c "CREATE TABLE IF NOT EXISTS advanced_chats (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    file_path TEXT NOT NULL,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    pdf_path TEXT,
+    pdf_name TEXT,
+    pdf_uuid TEXT
+);"
 
 # Set up Conda environment
 echo "Setting up conda environment..."
@@ -45,10 +65,6 @@ else
     echo "No requirements.txt found"
 fi
 
-sudo -u "azureuser tee /home/azureuser/$REPO_NAME/.env" <<EOF
-KEY_VAULT_NAME=$KEY_VAULT_NAME
-EOF
-
 # Create systemd services
 echo "Creating systemd services..."
 cat <<EOF | sudo tee /etc/systemd/system/chromadb.service
@@ -60,7 +76,23 @@ After=network.target
 Type=simple
 User=$USER
 WorkingDirectory=$HOME_DIR/$REPO_NAME
-ExecStart=$HOME_DIR/miniconda3/envs/project/bin/chroma run --path $HOME_DIR/$REPO_NAME/chroma_db --host 0.0.0.0
+ExecStart=$HOME_DIR/miniconda3/envs/project/bin/chroma run --path $HOME_DIR/$REPO_NAME/chroma_db
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+cat <<EOF | sudo tee /etc/systemd/system/backend.service
+[Unit]
+Description=backend
+After=network.target
+
+[Service]
+Type=simple
+User=$USER
+WorkingDirectory=$HOME_DIR/$REPO_NAME
+ExecStart=$HOME_DIR/miniconda3/envs/project/bin/uvicorn backend:app --reload --port 5000
 Restart=always
 
 [Install]
